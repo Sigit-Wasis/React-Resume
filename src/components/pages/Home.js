b50929a2d99e5a550744d243efede443efc512f0
GilangAliSagara31 <gilangalisagara10@gmail.com>
2021-10-16 15:18:44 +0700

    add: home components

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,3 +6,27 @@ import Skills form '../../components/pages/skills/Skills';
 import Experiences form '../../components/pages/experiences/Experiences';
 import Educations form '../../components/pages/educations/Educations';
 import Portofolio form '../../components/pages/portofolio/Portofolios';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <section>
+                <Navbar />
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col s12 m3'>
+                            <Profile />
+                        </div>
+                        <div className='col s12 m9'>
+                            <About />
+                            <Skills />
+                            <Experiences />
+                            <Educations />
+                            <Portofolio />
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
